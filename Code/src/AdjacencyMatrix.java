d815c4e42c01541ea6c6fac14afd137c40a4719b
Marty Marek <marto@DESKTOP-4PGE7LG>
2021-04-03 15:26:10 +1100

    Updated SuperMatrix
    Now works properly and can be tested using the printMatrix()

--- Code/src/AdjacencyMatrix.java
+++ Code/src/AdjacencyMatrix.java
@@ -10,22 +10,71 @@ import java.io.PrintWriter;
  */
 public class AdjacencyMatrix extends AbstractGraph
 {
+	
+	//this is our matrix (of boolean to track edges) 
+	SuperMatrix<Boolean> adjMatrix; 
 
 	/**
 	 * Contructs empty graph.
 	 */
     public AdjacencyMatrix() {
-    	// Implement me!
+    	super();
+    	
+    	//The adjacency matrix will use the same map coordinates to represent 
+    	//rows and columns in our matrix. Therefore, we don't need a second map
+    	
+    	//instantiate the matrix 
+    	adjMatrix  = new SuperMatrix<Boolean>(); // initial size of 10 x 10 will be created
+    	
     } // end of AdjacencyMatrix()
 
 
     public void addVertex(String vertLabel) {
-        // Implement me!
+    	//create and add a new vertex to the map if vertLabel does not already exist
+    	if (!map.containsKey(vertLabel)) {
+    		
+    		//put the new vertlabel as a new key and connect it with a new vertex 
+    		//the new vertex is also given the current index from our matrix
+    		map.put(vertLabel, new Vertex(adjMatrix.getCurrentRowIndex()));
+    		adjMatrix.addRow();
+
+    	}
     } // end of addVertex()
 
 
     public void addEdge(String srcLabel, String tarLabel) {
-        // Implement me!
+    	//don't allow self loops
+    	if (srcLabel.equals(tarLabel)) {
+    		return;
+    	}
+        
+    	try {
+	    	//first we need to check that both labels exist
+	    	if (map.containsKey(srcLabel) && map.containsKey(tarLabel) ) {
+	    		
+	    		//now we get the source vertex and its index in the array
+	    		Vertex sourceVertex = map.get(srcLabel);
+	    		Vertex targetVertex = map.get(tarLabel);
+	    		
+	    		int sourceIndex = sourceVertex.getIndexPointer();
+	    		int targetIndex = targetVertex.getIndexPointer();
+	    		
+	    		//source is the row and target is the column in our matrix 
+	    		Boolean edge = new Boolean(true);
+	    		adjMatrix.setObject(sourceIndex, targetIndex, edge);
+	    		
+	    				    		
+	    	}
+    	}
+	    catch (ArrayIndexOutOfBoundsException aie) {
+	    	//something went wrong with accessing the array with that index number
+	    }
+    	catch (NullPointerException npe) {
+    		//something went wrong with accessing the linked list
+    	}
+	    catch (Exception e) {
+	    	//Something else has gone wrong
+	    }
     } // end of addEdge()
 
 
@@ -53,12 +102,18 @@ public class AdjacencyMatrix extends AbstractGraph
 
 
     public void printVertices(PrintWriter os) {
-        // Implement me!
+    	//testing only right now
+    	for (String n: map.keySet()) {
+    		System.out.print(n + " ");
+    		System.out.print(map.get(n).getIndexPointer() + " ");
+    		System.out.println(map.get(n).getState().toString());
+    		
+    	}
     } // end of printVertices()
 
 
     public void printEdges(PrintWriter os) {
-        // Implement me!
+        adjMatrix.printMatrix();
     } // end of printEdges()
 
 } // end of class AdjacencyMatrix
--- Code/src/SuperMatrix.java
+++ Code/src/SuperMatrix.java
@@ -5,15 +5,21 @@ public class SuperMatrix<Obj> {
 	
 	SuperArray<SuperArray<Obj>> rows;
 	
+	//keeps track of how many rows we have in the matrix
+	private int rowCount;
+	
+	//keeps track of how many columns we have 
+	private int columnCount;
+	
 	//the default constructor will create a matrix of default array size (ie. 10 by 10)
 	public SuperMatrix() {
 		rows = new SuperArray<SuperArray<Obj>>();
 		
-		int i = 0;
-		
-		while (i++ < rows.getLength()) {
-			rows.add(new SuperArray<Obj>());
-		}
+		//for default matrix we start with no rows but column size will be 10
+		//Please Note: This is just the allocated storage for the matrix
+		//NOT the actual Matrix size
+		rowCount = 0;
+		columnCount = 10;		
 		
 	}
 	
@@ -21,12 +27,20 @@ public class SuperMatrix<Obj> {
 	public SuperMatrix(int numRows, int numColumns) throws NegativeArraySizeException {
 		rows = new SuperArray<SuperArray<Obj>>(numRows);
 		
-		int i = 0;
-		
-		while (i++ < numRows) {
-			rows.add(new SuperArray<Obj>(numColumns));
-		}
+		rowCount = numRows;
+		columnCount = numColumns;
 	}
+	
+	//returns the current row index
+	public int getCurrentRowIndex() {
+		return rows.getCurrentIndex();
+	}
+	
+	//returns the current column index (same as rows for symmetrical matrices) 
+	public int getCurrentColumnIndex() {
+		return rows.getObject(0).getCurrentIndex(); //use the first row to get the column index 
+	}
+	
 	
 	//returns the current object at given index
 	//rows and columns start at 0
@@ -36,19 +50,42 @@ public class SuperMatrix<Obj> {
 	
 	//sets the object at a given row/column
 	//rows and columns start at 0
-	public void setObject(int row, int column, Obj obj) {
+	public void setObject(int row, int column, Obj obj) throws ArrayIndexOutOfBoundsException {
 		rows.getObject(row).setObject(column, obj);
 		
+		return;
 	}
+	
+	//add default sized row (10) to the Matrix 
+	public void addRow() {
+		//adding a row is simple just create a new array and add it
+		SuperArray<Obj> newRow = new SuperArray<Obj>();
+		rows.add(newRow);
+	}
+	
+	//add a row of given size to the matrix 
+	public void addRow(int size) {
+		//adding a row is simple just create a new array and add it
+		SuperArray<Obj> newRow = new SuperArray<Obj>(size);
+	}
+	
+	
 	
 	//Please note: this is for testing purposes only, don't run this with a large matrix
 	public void printMatrix() {
 		
 		int i, j = 0;
 		
-		for (i = 0; i < rows.getLength(); i++) {
+		for (i = 0; i < rows.getTotalItems(); i++) {
 			for (j = 0; j < rows.getObject(i).getLength(); j++) {
-				System.out.print(rows.getObject(i).getObject(j));
+				if (rows.getObject(i).getObject(j) == null ||
+						(Boolean)rows.getObject(i).getObject(j) == false) {
+					System.out.print("0 ");
+					
+				}
+				else {
+					System.out.print("1 ");
+				}
 			}
 			System.out.println();
 		}
