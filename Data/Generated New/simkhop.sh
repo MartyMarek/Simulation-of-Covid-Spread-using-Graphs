38db784104dacf21774edb46655512d4bbb4de12
Marty Marek <marto@DESKTOP-4PGE7LG>
2021-04-14 16:55:39 +1000

    unix bash file update for simulation (it now combines output files)

--- Data/Generated New/simkhop.sh
+++ Data/Generated New/simkhop.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/bash
 
-for i in {1..9}
+for i in {1..3}
 do
-  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f sf${i}.net -o k${i}list.out adjlist < khoptest2345.dat
-  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f sf${i}.net -o k${i}mat.out adjmat < khoptest2345.dat
-  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f sf${i}.net -o k${i}inc.out incmat < khoptest2345.dat
+  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f sf${i}.net -o k${i}list.out adjlist < khoptest234.dat
+  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f sf${i}.net -o k${i}mat.out adjmat < khoptest234.dat
+  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f sf${i}.net -o k${i}inc.out incmat < khoptest234.dat
   echo "Scale Free Iteration $i is done"
-  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f er${i}.net -o k${i}list.out adjlist < khoptest2345.dat
-  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f er${i}.net -o k${i}mat.out adjmat < khoptest2345.dat
-  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f er${i}.net -o k${i}inc.out incmat < khoptest2345.dat
+  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f er${i}.net -o k${i}list.out adjlist < khoptest234.dat
+  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f er${i}.net -o k${i}mat.out adjmat < khoptest234.dat
+  java -cp .:jopt-simple-5.0.2.jar RmitCovidModelling -f er${i}.net -o k${i}inc.out incmat < khoptest234.dat
   echo "ER Iteration $i is done."
 done
+
+echo "Combining Files..."
+
+for j in {1..3}
+do
+  cat k${i}list.out >> final_khop_adjlist.out
+  cat k${i}mat.out >> final_khop_adjmat.out
+  cat k${i}inc.out >> final_khop_incmat.out
+done
+
+unix2dos final_khop_adjlist.out final_khop_adjlist.txt
+unix2dos final_khop_adjmat.out final_khop_adjmat.txt
+unix2dos final_khop_incmat.out final_khop_incmat.txt
+
+ech "Simulation Complete"
